3290b3da0edd05f5227c5c217a43103f71ce164f
 Ivan Ushakov <ivan.ushakov@gmail.com>
2018-11-22 14:30:23 +0300

    Initial Commit

---
+++ HexMapEditor/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  HexMapEditor
+//
+//  Created by  Ivan Ushakov on 22/11/2018.
+//  Copyright © 2018  Ivan Ushakov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
